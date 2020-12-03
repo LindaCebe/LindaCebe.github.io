0958152b9ea9a84160bac743e2061438d067558b
Linda Cebecauerova <lindacebecauerova@Lindas-MacBook-Pro.local>
2020-12-03 12:30:42 +0000

    p5 js project

---
+++ assets/draw.js
@@ -0,0 +1,40 @@
+
+        var r = 2;
+        var posX = 200,
+        posY = 200;
+
+        var prevPosY = 200;
+        var prevPosX = 200;
+
+        let words = [1, 2, 3, 4];
+
+        function setup() {
+            let canvas = createCanvas(400, 400);
+            canvas.parent('p5Canvas');
+            strokeWeight(3);
+        }
+
+        function draw() {
+
+            frameRate(15);
+            r = random(words);
+            //up
+            if (r == 1) {
+                posY+=10;
+            }
+            //down
+            if (r == 2) {
+                posY-=10;
+            }
+            //left
+            if (r == 3) {
+                posX-=10;
+            }
+            //right
+            if (r == 4) {
+                posX+=10;
+            }
+            line(prevPosX,prevPosY,posX,posY);
+            prevPosX = posX;
+            prevPosY = posY;
+        }
