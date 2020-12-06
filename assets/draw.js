a7853b03f017a927e7df01aec0f69788597e0747
Linda Cebecauerova <lindacebecauerova@Lindas-MacBook-Pro.local>
2020-12-06 23:10:20 +0000

    index page

--- assets/draw.js
+++ assets/draw.js
@@ -1,29 +1,72 @@
-let a = [];
-let b = [];
-
+var numberOfRipples, speedOfRipples, positionOfRipples;
+let ripple1;
+var i = -1;
+let rippless = [];
 
 function setup() {
     let canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent('p5Canvas');
-  
+    strokeWeight(1.2);
+  noFill();
 }
 
 function draw() {
-  background(220);
-  if(mouseX != pmouseX){
-    a.push(mouseX);
-    b.push(mouseY);
+   background(50, 111, 168);
+  if(i > 0){
+    rippless[i].create();
+    rippless[i].move();
   }
+}
+
+function mouseClicked(){
+  positionOfRipples = createVector(mouseX, mouseY);
+   rippless.push(new Ripples(positionOfRipples));
+  i++;
   
-  for(var i = 1; i < a.length+1; i++){
-    fill(0,0,0,i);
-    line(a[i-1],b[i-1],a[i],b[i]); 
+}
+
+
+
+
+
+
+
+        var lastCircleSize;
+// var b = false;
+
+class Ripples{
+  constructor(position){
+    this.size = floor(random(7,20));
+    this.speed = floor(random(1,5));
+    this.positionX = position.x;
+    this.positionY = position.y;
+  this.b = false;
+    this.bigger = 0;
+    
     
   }
   
-  if(a.length>100){
-    a.shift();
-    b.shift();
+  create(){
+    
+     //background(220);
+    //background(220);
+    for(var i = 1; i < this.size+1; i++){
+      stroke(0, 67, 125);
+      circle(this.positionX,this.positionY,this.bigger*i*this.speed);
+      stroke(98, 142, 179);
+      circle(this.positionX,this.positionY,this.bigger*i*this.speed-5);
+    }
+    //this.bigger+=20;
+    
+  }
+  
+  move(){
+    this.bigger+=0.1;
+    
   }
   
 }
+
+    function windowResized(){
+        resizeCanvas(windowWidth, windowHeight);
+    }
